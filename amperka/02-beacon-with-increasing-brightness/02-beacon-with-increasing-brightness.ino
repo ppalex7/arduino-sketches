2cd8c861d24312a039ac7ffe431f4d01b6c7ec40
Alexandr Duplishchev <ppalex@yandex-team.ru>
2022-05-14 22:15:11 +0300

    допзадание: добавить в противофазу второй светодиод

--- amperka/02-beacon-with-increasing-brightness/02-beacon-with-increasing-brightness.ino
+++ amperka/02-beacon-with-increasing-brightness/02-beacon-with-increasing-brightness.ino
@@ -1,4 +1,5 @@
 #define LED_PIN 9
+#define LED2_PIN 3
 
 unsigned int voltage = 1;
 const unsigned int maxVoltage = 5;
@@ -9,7 +10,9 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   int output = 255 * voltage / maxVoltage;
+  int output2 = 255 * (maxVoltage - voltage) / maxVoltage;
   analogWrite(LED_PIN, output);
+  analogWrite(LED2_PIN, output2);
   delay(166);
 
   if (++voltage > maxVoltage) {
