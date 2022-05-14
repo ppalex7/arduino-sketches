131769b827b91218669aaae96c13760b37fe77cd
Alexandr Duplishchev <ppalex@yandex-team.ru>
2022-05-14 22:22:21 +0300

    добавить отладочный вывод текущих значений на серийный порт

--- amperka/02-beacon-with-increasing-brightness/02-beacon-with-increasing-brightness.ino
+++ amperka/02-beacon-with-increasing-brightness/02-beacon-with-increasing-brightness.ino
@@ -5,12 +5,18 @@ unsigned int voltage = 1;
 const unsigned int maxVoltage = 5;
 
 void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int output = 255 * voltage / maxVoltage;
   int output2 = 255 * (maxVoltage - voltage) / maxVoltage;
+  Serial.print("v1: ");
+  Serial.print(output);
+  Serial.print(", v2: ");
+  Serial.print(output2);
+  Serial.println();
   analogWrite(LED_PIN, output);
   analogWrite(LED2_PIN, output2);
   delay(166);
