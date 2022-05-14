86b68e89957e896fc4eab5d4e22fb24a0f13df36
Alexandr Duplishchev <ppalex@yandex-team.ru>
2022-05-14 22:04:09 +0300

    допзадание: менять напряжение 0,1,2,3,4,5 в течение секунды

--- amperka/02-beacon-with-increasing-brightness/02-beacon-with-increasing-brightness.ino
+++ amperka/02-beacon-with-increasing-brightness/02-beacon-with-increasing-brightness.ino
@@ -1,16 +1,18 @@
 #define LED_PIN 9
 
+unsigned int voltage = 1;
+const unsigned int maxVoltage = 5;
+
 void setup() {
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  analogWrite(LED_PIN, 85);
-  delay(250);
+  int output = 255 * voltage / maxVoltage;
+  analogWrite(LED_PIN, output);
+  delay(166);
 
-  analogWrite(LED_PIN, 170);
-  delay(250);
-
-  analogWrite(LED_PIN, 255);
-  delay(250);
+  if (++voltage > maxVoltage) {
+    voltage = 0;
+  }
 }
