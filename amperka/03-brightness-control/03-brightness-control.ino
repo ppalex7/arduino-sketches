4dc70b414245ef18ba94394be94dc4a656684ba0
Alexandr Duplishchev <ppalex@yandex-team.ru>
2022-05-19 00:05:58 +0300

    добавить отладочную печать текущего положения, с которой всяко нагляднее

--- amperka/03-brightness-control/03-brightness-control.ino
+++ amperka/03-brightness-control/03-brightness-control.ino
@@ -1,9 +1,16 @@
 #define CONTROL_PIN A0
 #define LED_PIN 9
 
+unsigned long lastDebugPrintTime;
+int previousRotation;
+
 void setup() {
   pinMode(CONTROL_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
+
+  Serial.begin(9600);
+  while(!Serial) {
+  }
 }
 
 void loop() {
@@ -14,4 +21,16 @@ void loop() {
   brightness = rotation / 4;
 
   analogWrite(LED_PIN, brightness);
+
+  unsigned long time = millis();
+  if (time - lastDebugPrintTime > 10000
+    || previousRotation != rotation
+  ) {
+    Serial.print(F("Current analog value is: "));
+    Serial.print(rotation);
+    Serial.print(F(", logged at "));
+    Serial.println(time);
+    lastDebugPrintTime = time;
+  }
+  previousRotation = rotation;
 }
