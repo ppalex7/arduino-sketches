7b39315eb0cabe25b35606b266fe203ce9636afe
Alexandr Duplishchev <ppalex@yandex-team.ru>
2022-05-28 15:08:01 +0300

    №6. Пульсар

---
+++ amperka/06-pulsar/06-pulsar.ino
@@ -0,0 +1,13 @@
+#define CONTROL_PIN 9
+
+void setup(){
+    pinMode(CONTROL_PIN, OUTPUT);
+}
+
+int brightness = 0;
+
+void loop() {
+    brightness = ++brightness % 256;
+    analogWrite(CONTROL_PIN, brightness);
+    delay(10);
+}
