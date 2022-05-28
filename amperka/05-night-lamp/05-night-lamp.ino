fa8d44ac5b0706e47b98b0a5b6835cdb38169dd0
Alexandr Duplishchev <ppalex@yandex-team.ru>
2022-05-28 13:48:41 +0300

    допзадание: зажигать второй светодиод при падении яркости в половину от порога

--- amperka/05-night-lamp/05-night-lamp.ino
+++ amperka/05-night-lamp/05-night-lamp.ino
@@ -1,9 +1,11 @@
 #define LED_PIN 13
+#define LED2_PIN 12
 #define LDR_PIN A1
 #define POT_PIN A0
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
+  pinMode(LED2_PIN, OUTPUT);
 }
 
 void loop() {
@@ -11,4 +13,5 @@ void loop() {
   int threshold = analogRead(POT_PIN);
 
   digitalWrite(LED_PIN, lightness < threshold ? HIGH : LOW);
+  digitalWrite(LED2_PIN, (lightness < threshold / 2) ? HIGH : LOW);
 }
