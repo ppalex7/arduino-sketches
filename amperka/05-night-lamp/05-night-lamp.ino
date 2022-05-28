762fbc4f10c165a44053fcfb2b09016f80b3b141
Alexandr Duplishchev <ppalex@yandex-team.ru>
2022-05-28 14:15:35 +0300

    допзадание: оставить логику включенимя той же, но светить тем ярче, чем меньше света. (NB! нужна перекомутация светодиодов на PWM пины)

--- amperka/05-night-lamp/05-night-lamp.ino
+++ amperka/05-night-lamp/05-night-lamp.ino
@@ -1,5 +1,5 @@
-#define LED_PIN 13
-#define LED2_PIN 12
+#define LED_PIN 11
+#define LED2_PIN 10
 #define LDR_PIN A1
 #define POT_PIN A0
 
@@ -12,6 +12,8 @@ void loop() {
   int lightness = analogRead(LDR_PIN);
   int threshold = analogRead(POT_PIN);
 
-  digitalWrite(LED_PIN, lightness < threshold ? HIGH : LOW);
-  digitalWrite(LED2_PIN, (lightness < threshold / 2) ? HIGH : LOW);
+  long brightness = map(lightness, 1023, 0, 0, 255);
+
+  analogWrite(LED_PIN, lightness < threshold ? brightness : 0);
+  analogWrite(LED2_PIN, (lightness < threshold / 2) ? brightness : 0);
 }
