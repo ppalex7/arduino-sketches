7717bfbb384d0f159bec44e205defd764a9c0ea2
Alexandr Duplishchev <ppalex@yandex-team.ru>
2023-05-25 00:35:11 +0300

    допзадание 2: сделать, чтобы огонек бегал только по средним 4 сегментам

--- amperka-examples/07-running-light/07-running-light.ino
+++ amperka-examples/07-running-light/07-running-light.ino
@@ -1,5 +1,5 @@
-#define FIRST_LED_PIN 2
-#define LAST_LED_PIN 11
+#define FIRST_LED_PIN 5
+#define LAST_LED_PIN 8
 
 void setup() {
   for (int pin = FIRST_LED_PIN; pin <= LAST_LED_PIN; ++pin) {
@@ -11,7 +11,7 @@ void setup() {
 int prevPin = LAST_LED_PIN;
 void loop() {
   unsigned long int ms = millis();
-  int pin = FIRST_LED_PIN + (ms / 1000) % 10;
+  int pin = FIRST_LED_PIN + (ms / 120) % (LAST_LED_PIN - FIRST_LED_PIN + 1);
   if (prevPin != pin) {
     digitalWrite(prevPin, LOW);
     digitalWrite(pin, HIGH);
