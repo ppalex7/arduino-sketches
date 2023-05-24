9c5024f98b356cc039fb6c566699275822f8da02
Alexandr Duplishchev <ppalex@yandex-team.ru>
2023-05-25 00:30:25 +0300

    №7. Бегущий огонек по ощущениям это совсем не 10 миллисекунд

---
+++ amperka-examples/07-running-light/07-running-light.ino
@@ -0,0 +1,17 @@
+#define FIRST_LED_PIN 2
+#define LAST_LED_PIN 11
+
+void setup() {
+  for (int pin = FIRST_LED_PIN; pin <= LAST_LED_PIN; ++pin) {
+    pinMode(pin, OUTPUT);
+  }
+
+}
+
+void loop() {
+  unsigned long int ms = millis();
+  int pin = FIRST_LED_PIN + (ms / 120) % 10;
+  digitalWrite(pin, HIGH);
+  delay(10);
+  digitalWrite(pin, LOW);
+}
