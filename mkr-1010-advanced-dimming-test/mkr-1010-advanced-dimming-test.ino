365426b1f6b2334d8e36519542e9d0d9ed981fb4
Alexandr Duplishchev <ppalex@yandex-team.ru>
2023-08-05 19:45:34 +0300

    добавить инфу о состоянии часов для переферии таймеров (и ненужную попытку включения, так как нашлась штатная в недрах ядра ардуино)

--- mkr-1010-advanced-dimming-test/mkr-1010-advanced-dimming-test.ino
+++ mkr-1010-advanced-dimming-test/mkr-1010-advanced-dimming-test.ino
@@ -5,19 +5,23 @@
 
 
 void setup() {
+  // 16.6.2.6 Peripheral Clock Default state (according to datasheet):
+    // CLK_PORT_APB — Enabled
+    // CLK_TCx_APB — Disabled
+    // CLK_TCCx_APB — Disabled
+
   // 31.5 Timer/Counter for Control Applications. Product dependencies:
   // 31.5.1. In order to use I/O lines of this peripheral, the I/O pins must be configured using the I/O Pin Controller (PORT)
   PORT->Group[ g_APinDescription[ PIN_L3_W ].ulPort ].PINCFG[ g_APinDescription[ PIN_L3_W ].ulPin ].bit.PMUXEN = 1;
 
   // 31.5.3 The TCC bus clocks (CLK_TCCx_APB) can be enabled and disabled in the Power Manager module.
   // The default state of CLK_TCCx_APB can be found in the Peripheral Clock Masking.
+  PM->APBCMASK.reg |= PM_APBCMASK_TCC0; // excess: it was already configured by init function in wiring.c
   // A generic clock (GCLK_TCCx) is required to clock the TCC.
   // This clock must be configured and enabled in the generic clock controller before using the TCC.
   // Note that TCC0 and TCC1 share a peripheral clock generator.
   // The generic clocks (TCLK_TCCx) are asynchronus to the bus clock (CLK_TCCx_APB).
   // Due to this asynchronicity, writing certain registers will require synchronization between the clock domains.
-
-
 }
 
 void loop() {
